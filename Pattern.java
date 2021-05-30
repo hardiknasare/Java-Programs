ffdf6a97f0b42c06da8834d390a6d8355be70934
Hardik Nasare <83773987+hardiknasare@users.noreply.github.com>
2021-05-30 15:06:23 +0530

    Java program to print the following pattern on the console
    *
    * *
    * * *
    * * * *
    * * * * *

---
+++ Pattern.java
@@ -0,0 +1,23 @@
+/*1:Java program to print the following pattern on the console
+*
+* *
+* * *
+* * * *
+* * * * * 
+*/
+public class Pattern {
+
+	public static void main(String[] args) {
+
+		for(int i=0;i<5;i++)
+		{
+			for(int j=0;j<=i;j++)
+			{
+				System.out.print("* ");
+			}
+			System.out.println();
+		}
+
+	}
+
+}
